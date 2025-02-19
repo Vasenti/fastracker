8cd2a34d82ea4c70b48cfd02b2a92c80f2f14159
Daniel Ledesma <oscar.ledesma@fravega.com.ar>
2025-02-19 15:20:43 -0300

    fix

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -33,14 +33,6 @@ const MapCenterUpdater = ({ center }: { center: LatLngExpression }) => {
     return null;
 };
 
-const customMarker = new L.Icon({
-    iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41],
-});
-
 const createNumberedMarker = (number: number) => {
     return new L.DivIcon({
         className: "custom-marker",
